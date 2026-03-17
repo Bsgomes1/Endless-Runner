ce4399ab962b0ed65c9505cd052e7da2d030d89b
Benjamin Saavedra Gomes <bsgomes@ucsc.edu>
2026-03-16 21:25:49 -0700

    Adjust sound volume to minimum in Play scene

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,6 +5,7 @@ class Play extends Phaser.Scene {
 
     create() {
         this.sound.volume = 0.01
+        this.sound.setVolume(0.01)
         // Display starfield
         this.starfield = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'starfield').setOrigin(0, 0)
 
