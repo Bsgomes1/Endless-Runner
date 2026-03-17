d6e22c0222a0baddf07deed52f20f3e6fe885255
Benjamin Saavedra Gomes <bsgomes@ucsc.edu>
2026-03-16 22:46:01 -0700

    Update Play.js

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,7 +4,7 @@ class Play extends Phaser.Scene {
     }
 
     create() {
-        this.sound.setVolume(0.1)
+        this.sound.setVolume(0.05)
         // Display starfield
         this.starfield = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'starfield').setOrigin(0, 0)
 
